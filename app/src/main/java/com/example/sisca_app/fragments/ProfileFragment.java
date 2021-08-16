e7171d298ed745d05b6aedc6809002a36f896c1d
aryawiratama18 <NAwiratama@gmail.com>
2021-08-16 07:13:33 +0700

    Open Gallery and camera dialog

--- app/src/main/java/com/example/sisca_app/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/sisca_app/fragments/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.sisca_app.fragments;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,6 +19,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,19 +36,34 @@ import com.example.sisca_app.LoginActivity;
 import com.example.sisca_app.Models.UsersModel;
 import com.example.sisca_app.R;
 import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment {
@@ -54,13 +73,16 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    private TextView patientFullName,patientNickName,patientAddress,patientAge,patientDoctorName;
-    private Button showQrButton,logoutButton;
+    private TextView patientFullName, patientNickName, patientAddress, patientAge, patientDoctorName;
+    private Button showQrButton, logoutButton;
     private Dialog qrDialog;
-    private ImageView qrOutput,patientImage;
+    private ImageView qrOutput, patientImage;
     private FirebaseAuth fAuth;
     private FirebaseFirestore fStore;
-    private String userId,imageURL;
+    private String userId, imageURL;
+    private Uri imageUri = null;
+    public static final int CAMERA_CODE = 200;
+    public static final int GALLERY_CODE = 100;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -82,6 +104,9 @@ public class ProfileFragment extends Fragment {
         userId = fAuth.getCurrentUser().getUid();
         qrDialog = new Dialog(getContext());
 
+        askPermission(); // asking for permissions
+
+
         DocumentReference documentReference = fStore.collection("users").document(userId);
         documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
             @Override
@@ -93,14 +118,20 @@ public class ProfileFragment extends Fragment {
                 patientDoctorName.setText("dr." + value.getString("dName"));
 
                 imageURL = value.getString("imageURL");
-                if(imageURL.equals("default"))
-                {
+                if (imageURL.equals("default")) {
                     patientImage.setImageResource(R.drawable.default_profile);
-                } else
-                {
+                } else {
                     Glide.with(getActivity().getApplicationContext()).load(imageURL).into(patientImage);
                 }
 
+            }
+
+        });
+
+        patientImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImage();
             }
         });
 
@@ -111,11 +142,11 @@ public class ProfileFragment extends Fragment {
 
                 String data = patientFullName.getText().toString() + "," + patientAge.getText().toString() + "," + "patient" + "," + patientAddress.getText().toString() + "," + patientDoctorName.getText().toString();
                 MultiFormatWriter writer = new MultiFormatWriter();
-                try{
-                    BitMatrix matrix = writer.encode(data, BarcodeFormat.QR_CODE,200,200);
+                try {
+                    BitMatrix matrix = writer.encode(data, BarcodeFormat.QR_CODE, 200, 200);
                     BarcodeEncoder encoder = new BarcodeEncoder();
                     Bitmap bitmap = encoder.createBitmap(matrix);
-                    loadPhoto(bitmap,600,600);
+                    loadPhoto(bitmap, 600, 600);
 
                 } catch (WriterException e) {
                     e.printStackTrace();
@@ -128,29 +159,118 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getActivity(),LoginActivity.class));
+                startActivity(new Intent(getActivity(), LoginActivity.class));
                 getActivity().finish();
             }
         });
         return view;
     }
 
+    private void changeImage() {
+        String[] options = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Choose an Option");
 
-    private void loadPhoto(Bitmap bitmap, int width, int height)
-    {
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        openCamera();
+                        break;
+                    case 1:
+                        openGallery();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_CODE);
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Temp Pick");
+        values.put(MediaStore.Images.Media.TITLE, "Temp Desc");
+        imageUri = getContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, CAMERA_CODE);
+    }
+
+    private void askPermission() {
+        Dexter.withContext(getContext())
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+
+            }
+        }).check();
+    }
+
+
+    private void loadPhoto(Bitmap bitmap, int width, int height) {
         Bitmap tempImageView = bitmap;
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         AlertDialog imageDialog = builder.create();
         LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = inflater.inflate(R.layout.popup_qr,(ViewGroup) getActivity().findViewById(R.id.qr_container));
+        View layout = inflater.inflate(R.layout.popup_qr, (ViewGroup) getActivity().findViewById(R.id.qr_container));
         ImageView image = (ImageView) layout.findViewById(R.id.qr_output);
         image.setImageBitmap(tempImageView);
         imageDialog.setView(layout);
 
         imageDialog.show();
         imageDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        imageDialog.getWindow().setLayout(width,height);
+        imageDialog.getWindow().setLayout(width, height);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            String filepath = "Photos/" + "patientprofile_" + userId;
+
+            StorageReference reference = FirebaseStorage.getInstance().getReference(filepath);
+            reference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> task = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+
+                    task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String imageURL = uri.toString();
+
+                            DocumentReference userReference = fStore.collection("users").document(userId);
+                            HashMap<String, Object> hashMap = new HashMap<>();
+                            hashMap.put("imageURL", imageURL);
+                            userReference.update(hashMap);
+                        }
+                    });
+                }
+            });
+
+        }
+    }
 }
